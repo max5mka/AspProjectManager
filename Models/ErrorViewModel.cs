22969dd74bb505751286d7c365d6fc718ffbfe0c
max5mka <max5mka01@gmail.com>
2025-11-16 14:12:59 +0700

    Initial commit: Created ASP.NET Core project with a simple Project model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspProjectManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace AspProjectManager.Models
+{
+    public class Project
+    {
+        public int Id { get; }
+        public required string Name { get; set; }
+
+        public Project(string name)
+        {
+            Name = name;
+        }
+
+        
+    }
+}
